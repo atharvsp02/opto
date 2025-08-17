1efbea86b1680a072b8d8e786c78603e00ee4130
atharv <atharvsp002@gmail.com>
2025-08-17 11:41:33 +0530

    Added changes in .env

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: OPTO_KEY,
-  authDomain: "opto-f4913.firebaseapp.com",
-  projectId: "opto-f4913",
-  storageBucket: "opto-f4913.firebasestorage.app",
-  messagingSenderId: "929323026408",
-  appId: "1:929323026408:web:18b436a7fafececad4865d",
-  measurementId: "G-F7DYEST0YV"
+  apiKey: import.meta.env.VITE_OPTO_KEY,
+  authDomain: import.meta.env.VITE_OPTO_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_OPTO_PROJECTID,
+  storageBucket: import.meta.env.VITE_OPTO_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_OPTO_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_OPTO_APPID,
+  measurementId: import.meta.env.VITE_OPTO_MEASUREMENTID,
 };
 
 // Initialize Firebase
